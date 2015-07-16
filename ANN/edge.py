fixed NN error calculation

ANN/edge.py
@@ -1,5 +1,5 @@
 import numpy as np
-np.random.seed(1)
+# np.random.seed(1)
 class edge(object):
     """docstring for edge"""
     def __init__(self,originNode,terminalNode,weight = None):
@@ -10,7 +10,7 @@ class edge(object):
         self.terminalNode = terminalNode
         self.originNode   = originNode
 
-    def update_weight(self,eta = 10):
+    def update_weight(self,eta = 100):
         self.weight += eta*self.terminalNode.delta*self.terminalNode.deriv*self.originNode.val
 
     def forward_propogate_val(self):

ANN/neural_network.py
@@ -33,7 +33,6 @@ class neural_network(object):
                 for originY,originNode in enumerate(originLayer):
                     edgeName = edge_name(originX,originY,terminalX,terminalY)
                     self.edges[edgeName].forward_propogate_val()
-
                 terminalNode.update_val()
 
     def initialize_edges(self):
@@ -60,14 +59,17 @@ class neural_network(object):
             output.append(node.val)
         return output
 
-    def train(self,trainingData,acceptableError = 10):
+    def train(self,trainingData,acceptableError = .29):
         """
             training data is a list of the form
             [[[input points],[ output points]],...]
         """
-        # while np.mean(np.array(self.get_node_errors())**2) > acceptableError:
-        for i in range(50):
-            # print self.get_node_errors()
+        n = len(trainingData)
+        error = 1
+        peta = 1
+        while error > acceptableError:
+            print error
+            error = 0
             for example in trainingData:
                 self.feed_forward(input = example[0])
                 desiredOutput = example[1]
@@ -75,7 +77,7 @@ class neural_network(object):
                     print "Output length doesnt match the number of out nodes"
                     return
                 for i,node in enumerate(self.layers[-1]):
-                    node.delta = desiredOutput[i]-node.val
+                    node.delta = desiredOutput[i]-node.raw
                 for terminalX in range(1,self.numLayers):
                     originX       = terminalX-1
                     originLayer   = self.layers[originX]
@@ -86,17 +88,21 @@ class neural_network(object):
                             edgeName = edge_name(originX,originY,terminalX,terminalY)
                             self.edges[edgeName].back_propogate_error()
                 for edge in self.edges.values():
-                    edge.update_weight(eta = 1000-i)
+                    edge.update_weight(eta = peta)
+                error+=np.linalg.norm(self.get_output()-np.array(desiredOutput))
+            error*=1/(2.*n)
+            peta = 5*error
 
+    def show(self):
+        pass
 
 #creates a name for an edge that goes from neuron x1,y1 to x2,y2
 def edge_name(x1,y1,x2,y2):
     return(str(x1)+str(y1)+','+str(x2)+str(y2))
 
-def generate_neural_network(numInputs,NumProcessingNodes,numOutputs):
+def generate_neural_network(layerSizes):
     """ This generates a feed forward neural network with 3 layers """
-    inputLayer      = [node() for i in range(numInputs)]
-    ProccesingLayer = [node() for i in range(NumProcessingNodes)]
-    outputLayer     = [node() for i in range(numOutputs)]
-    layers          = [inputLayer,ProccesingLayer,outputLayer]
+    layers = []
+    for layerSize in layerSizes:
+        layers.append([node() for i in range(layerSize)])
     return(neural_network(layers))